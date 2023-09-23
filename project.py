38df0ca858c4623b4c16e9ef1598ca9094c09d0e
Vaibhav Singh <91045085+vaibhavsiingh00@users.noreply.github.com>
2023-09-23 22:44:12 +0530

    Add files via upload

---
+++ project.py
@@ -0,0 +1,38 @@
+import cv2 
+
+face_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_frontalface_default.xml')
+eye_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_eye.xml')
+
+cap = cv2.VideoCapture(0)
+
+while True:
+    # Capture frame-by-frame
+    ret, img = cap.read()
+    # Our operations on the frame come here
+    gray = cv2.cvtColor(img, cv2.COLOR_RGB2GRAY)
+    # Detects faces of different sizes in the input image
+    faces = face_cascade.detectMultiScale(gray, 1.3, 5)
+    for (x,y,w,h) in faces:
+        # To draw a rectangle in a face
+        cv2.rectangle(img,(x,y),(x+w,y+h),(255,255,0),2)
+        roi_gray = gray[y:y+h, x:x+w]  # type: ignore
+        roi_color = img[y:y+h, x:x+w]
+        # Detects eyes of different sizes in the input image
+        eyes = eye_cascade.detectMultiScale(roi_gray)
+        #To draw a rectangle in eyes
+        for (ex,ey,ew,eh) in eyes:
+            cv2.rectangle(roi_color,(ex,ey),(ex+ew,ey+eh),(0,127,255),2)
+            #text = "Eyes Detected"
+            #`cv2.putText(img, "Eye Detected",(ex,ey),cv2.FONT_HERSHEY_SIMPLEX, 0.7, (0, 0, 255))
+        # Display face detected
+        cv2.putText(img, "Face Detected", (x, y-10), cv2.FONT_HERSHEY_SIMPLEX, 0.9, (36,255,12), 2)
+    # Display an image in a window
+    cv2.imshow('img',img)
+    # Wait for Esc key to stop
+    k = cv2.waitKey(30) & 0xff
+    if k == 27:
+        break
+# When everything done, release the capture
+cap.release()
+cv2.waitKey(0)
+cv2.destroyAllWindows()
